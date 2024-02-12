5db82c1a5bd2f7eaa555cdc7b6fdcefc665f20b0
Bedizz <turkyilmaz.bediz@gmail.com>
2024-02-12 15:29:33 +0100

    done

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import 'dotenv/config'
 import express from 'express';
 import { connectDatabase } from './db/client.js';
 import  countriesRouter  from './routes/countriesRouter.js';
+import cors from "cors";
+
 
 
 
@@ -10,6 +12,7 @@ const PORT = 3000 || process.env.PORT;
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.use('/api/countries',countriesRouter); // this is the middleware that will be used to route the request to the countriesRouter
 
